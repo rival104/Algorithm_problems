e89b2bcb4ee5a0efe240d6cb5b057c60bced94e0
rival104 <navid.rvl@gmail.com>
2019-11-21 19:40:51 -0500

    Solved the longest palindrome in more efficient soln

--- interview-problems/LongestPalindrome_Substring.js
+++ interview-problems/LongestPalindrome_Substring.js
@@ -3,6 +3,45 @@
  * @param {string} s
  * @return {string}
  */
+
+ /**
+ * expand method
+ */
+var longestPalindrome = function(s) {
+    let longest = s.substring(0,1);
+
+    for(let i = 0; i < s.length; i++){
+        if(!s || s.length === 1){
+            return s;
+        }
+        //palindrome with odd center
+        let expandedOdd = expand(s, i, i);
+        //palindrome with even center
+        let expandedEven = expand(s, i, i+1);
+
+        let expanded = "";
+        if(expandedEven.length > expandedOdd.length){
+            expanded = expandedEven
+        }else{
+            expanded = expandedOdd;
+        }
+
+        if(expanded.length > longest.length){
+            longest = expanded;
+        }
+    }
+
+    return longest;
+};
+
+function expand(str, begin, end){
+    while(begin >= 0 && end < str.length && str[begin] === str[end]){
+        begin--;
+        end++;
+    }
+    return str.substring(begin + 1, end);
+}
+
  //brute force method
 var longestPalindrome = function(s) {
     let subStr = "";
